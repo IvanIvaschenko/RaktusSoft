4f887a0157f8be1943b9baec1291c2c516de1e95
Ivan Ivaschenko <ivaschenko.ivan@ukr.net>
2017-05-16 20:24:31 +0300

    added CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,9 @@ var express 	= require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
 var jwt    = require('jsonwebtoken');
+var cors = require('cors');
 
-process.env.SECRET_KEY = 'verysecretkey';
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,39 +15,42 @@ var User =
     password: 'password'
   }
 
-data=[
+var  data=[
   {
     artist: 'the Beatles',
     concertPlace: 'Liverpool',
-    perfomansRecords: '0 concert per month'
+    perfomansRecords: '0 concert per month',
+    id: 1
   },
   {
     artist: 'the Kooks',
     concertPlace: 'Amsterdam',
-    perfomansRecords: '5 concert per month'
+    perfomansRecords: '5 concert per month',
+    id: 2
   },
   {
     artist: 'Arctic Monkeys',
     concertPlace: 'Paris',
-    perfomansRecords: '3 concert per month'
+    perfomansRecords: '3 concert per month',
+    id: 3
   },
   {
     artist: 'Linkin Park',
     concertPlace: 'Palermo',
-    perfomansRecords: '6 concert per month'
-  },
+    perfomansRecords: '6 concert per month',
+    id: 4
+  }
 ]
 
-app.get('/api/data', function(req, res) {
+app.get('/api/data', (req, res) => {
 	res.json(data);
 });
 
-app.post('/api/authenticate', function( req, res) {
-
-		if (User.username!==req.body.username && User.password !== req.body.password) {
+app.post('/api/auth', ( req, res) => {
+		if (User.username!==req.body.username || User.password !== req.body.password) {
 			res.json({ success: false, message: 'Authentication failed' });
     } else {
-		  var token = jwt.sign(User.username, process.env.SECRET_KEY);
+		  var token = jwt.sign(User.username, 'secret');
 			res.json({
 				success: true,
 				message: 'Enjoy your token!',
@@ -62,13 +61,14 @@ app.post('/api/authenticate', function( req, res) {
 	}
 );
 
-app.use(function(req, res, next) {
-
+app.use((req, res, next) => {
 	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
 
 	if (token) {
-		jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+		jwt.verify(token, 'secret', function(err, decoded) {
 			if (err) {
+        console.log(token);
+        console.log(err);
 				return res.json({ success: false, message: 'Failed to authenticate token.' });
 			} else {
 				req.decoded = decoded;
@@ -84,5 +84,41 @@ app.use(function(req, res, next) {
 
 });
 
+app.post('/api/data', (req, res)=>{
+  let obj ={
+    artist: req.body.artist,
+    concertPlace: req.body.place,
+    perfomansRecords: req.body.records,
+    id: '_' + Math.random().toString(36).substr(2, 9)
+  };
+
+  data.push(obj);
+  res.json({
+    success: true,
+    data
+  });
+});
+
+app.delete('/api/data', (req, res)=>{
+  let  result = data.filter((item)=>{return item.id !== req.body.id});
+  data = result;
+  res.json({
+    success: true,
+    data
+  })
+ }
+)
+
+app.put('/api/data', (req, res)=>{
+  item = data.find((item)=>{return item.id === req.body.id});
+  item.artist = req.body.artist;
+  item.concertPlace = req.body.place;
+  item.perfomansRecords = req.body.records;
+  res.json({
+    success: true,
+    data
+  })
+ }
+)
 
 app.listen(8080, ()=>'Server started');
